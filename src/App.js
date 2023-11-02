d073adff57e9bf12fff46ea955b2050153f54807
dothanhbing <thanhbinh01313@gmail.com>
2023-11-03 01:26:27 +0700

    build route & layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from './components/Layout';
+import { Fragment } from 'react';
+
 function App() {
     return (
-        <div className="">
-            <h1>React app</h1>
-        </div>
+        <Router>
+            <div className="">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Layout =
+                            route.layout === null ? Fragment : DefaultLayout;
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
---
+++ src/components/Layout/HeaderOnly/inndex.js
@@ -0,0 +1,13 @@
+import Header from './Header';
+function HeaderOnly() {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <div className="content"></div>
+            </div>
+        </div>
+    );
+}
+
+export default HeaderOnly;
