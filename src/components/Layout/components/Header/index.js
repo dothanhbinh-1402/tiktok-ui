d95e4c2400c24a9b5f20f7b159d8add64d9710f5
dothanhbing <thanhbinh01313@gmail.com>
2023-11-04 00:04:36 +0700

    UI Header #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,10 +1,33 @@
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
+import images from '~/assets/images';
+import { BsSearch } from 'react-icons/bs';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+import { BiLoaderCircle } from 'react-icons/bi';
+
 const cx = classNames.bind(styles);
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Tiktok"></img>
+                </div>
+                <div className={cx('search')}>
+                    <input
+                        placeholder='"Search accounts and videos'
+                        spellCheck={false}
+                    ></input>
+                    <button className={cx('clear')}>
+                        <AiOutlineCloseCircle />
+                    </button>
+                    <BiLoaderCircle className={cx('loading')} />
+                    <button className={cx('search-btn')}>
+                        <BsSearch />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
+            </div>
         </header>
     );
 }
