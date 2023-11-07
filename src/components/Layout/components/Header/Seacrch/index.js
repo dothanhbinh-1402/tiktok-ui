760d067f90d683b41a14cc84f5b29381470e76ec
dothanhbing <thanhbinh01313@gmail.com>
2023-11-08 04:03:16 +0700

    use Aixos call API

--- src/components/Layout/components/Header/Seacrch/index.js
+++ src/components/Layout/components/Header/Seacrch/index.js
@@ -3,6 +3,7 @@ import { BiLoaderCircle } from 'react-icons/bi';
 import HeadlessTippy from '@tippyjs/react/headless';
 import AccountItem from '~/components/AccountItem';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
+import * as searchServices from '~/apiServices/searchServices';
 import classNames from 'classnames/bind';
 import styles from './Search.module.scss';
 import { SearchIcon } from '~/components/Icons';
@@ -26,20 +27,14 @@ function Search() {
             setSearchResult([]);
             return;
         }
-        setLoading(true);
-        fetch(
-            `https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(
-                debounce,
-            )}&type=less`,
-        )
-            .then((res) => res.json())
-            .then((res) => {
-                setSearchResult(res.data);
-                setLoading(false);
-            })
-            .catch(() => {
-                setLoading(false);
-            });
+        const fetchApi = async () => {
+            setLoading(true);
+            const result = await searchServices.search(debounce);
+            setSearchResult(result);
+            setLoading(false);
+        };
+
+        fetchApi();
     }, [debounce]);
 
     const handleClear = () => {
